af9308658636a98051c7f7be80e9b39230c5ab33
Benasdarbas <joksas.elin@gmail.com>
2016-03-20 19:06:16 +0200

    Added files via upload

---
+++ pollutantmean.R
@@ -0,0 +1,26 @@
+### pract1 
+setwd("D:\\R\\coursera R\\R programing\\Prog assignment 1\\rprog-data-specdata")
+
+pollutantmean <- function(directory, pollutant, id = 1:332){
+ 
+   if(grep("specdata", directory) == 1) {
+    directory <- ("./specdata/")
+  }
+  
+  mean_vector <- c()
+  all_files <- as.character( list.files(directory) )
+  file_paths <- paste(directory, all_files, sep="")
+  for(i in id) {
+    current_file <- read.csv(file_paths[i], header=T, sep=",")
+    head(current_file)
+    pollutant
+    na_removed <- current_file[!is.na(current_file[, pollutant]), pollutant]
+    mean_vector <- c(mean_vector, na_removed)
+  }
+  result <- mean(mean_vector)
+  return(round(result, 3)) 
+}
+
+ 
+
+
